4b90aa34ac768ba41999e1c13049fccfc443aa0f
Dino <8dino2@gmail.com>
2022-04-02 22:45:35 -0400

    Renamed version file

--- build/mac/installer_create.sh
+++ build/mac/installer_create.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-hdiutil create AQ.dmg -srcfolder AQ_Install -volname AQ
+VERSION=$1
+ARCH=$2
+
+if [[ ${ARCH} != "intel" || ${ARCH} != "arm" ]]; then
+    echo "ARCH must be one of [intel | arm]"
+    exit 1
+fi
+
+## Populate AQ_Install directory
+mv ../../action AQ_Install/AQ.app/Contents/MacOS/
+cp q2probuilds/${ARCH}/q2proded q2probuilds/${ARCH}/q2pro AQ_Install/AQ.app/Contents/MacOS/
+cp q2probuilds/${ARCH}/gamex86_64.so AQ_Install/AQ.app/Contents/MacOS/action
+
+## Create dmg file
+hdiutil create action_quake-${VERSION}-mac_${ARCH}.dmg -srcfolder AQ_Install -volname "Action Quake"
+
+## Move action folder back
+mv AQ_Install/AQ.app/Contents/MacOS/action ../../
