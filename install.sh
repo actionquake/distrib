473eef375582d106e2bb1b4fd8c70a3ece9f1170
adean <8dino2@gmail.com>
2022-04-13 14:30:24 -0400

    Added optional symlink and debug info for troubleshooting

--- install.sh
+++ install.sh
@@ -11,4 +11,20 @@ if [[ $arch == "x86_64" ]]; then
     curl -L -o $target_file $download_link
     mkdir -p $target_dir
     tar xf $target_file -C $target_dir --strip-components=1
+    
+    echo "Creating symlinks to /usr/local/games, exit this script with CTRL-C if you do not have sudo access or do not want symlinks created..."
+    echo "Attempting this command: sudo ln -s $target_dir/q2pro /usr/local/bin/q2pro"
+    sudo ln -s $target_dir/q2pro /usr/local/bin/q2pro
+    echo "Attempting this command: sudo ln -s $target_dir/q2proded /usr/local/bin/q2proded"
+    sudo ln -s $target_dir/q2proded /usr/local/bin/q2proded
+
+else
+    echo "This installer only supports 64-bit Linux, if you feel this is in error, please create a Github issue"
+    echo "at this address: https://github.com/actionquake/distrib/issues"
+    echo "Paste the following information in the Github Issue you create:"
+    echo "Arch detected: " ${arch}
+    echo "uname output: " $(uname -a)
+    echo "Download link provided: " $download_link
+    exit 1
 fi
+exit 0
