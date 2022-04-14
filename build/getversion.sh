cffc8f1aafe748f21083da855d7f2e36f512fd76
adean <8dino2@gmail.com>
2022-04-14 13:59:01 -0400

    Removed changing dirs

--- build/getversion.sh
+++ build/getversion.sh
@@ -4,22 +4,18 @@
 package_version=$1
 
 q2pro () {
-  q2pro_path=q2pro
-  cd ${q2pro_path}
   q2pro_version=$(./version.sh  | head -n 1)
   echo "q2pro_version=${q2pro_version}"
 }
 
 q2admin () {
-  q2admin_path=q2admin
-  cd ${q2admin_path}
   q2admin_revision=$(grep REL_VERSION Makefile | head -n 1 | cut -f 2 -d '=')
   q2admin_version=$(git rev-parse --short HEAD)
   echo "q2admin_version=${q2admin_revision}~${q2admin_version}"
 }
 
 tng () {
-  tng_path=aq2-tng/source
+  tng_path=source
   cd ${tng_path}
   tng_revision=$(git show -s --format='%cd' --date=short)
   tng_version=$(git rev-parse --short HEAD)
@@ -27,8 +23,6 @@ tng () {
 }
 
 distrib () {
-  distrib=config
-  cd ${distrib}
   distrib_version=$(git rev-parse --short HEAD)
   echo "distrib_version=${distrib_version}"
 }
