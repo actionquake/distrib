10f9146b2a263ba1136e1777c4a8e667f7658f2e
adean <8dino2@gmail.com>
2022-04-18 14:56:04 -0400

    A continuous journey to get a good Linux install script

--- build/linux/dpkg/usr/local/share/aqtion/check_install.sh
+++ build/linux/dpkg/usr/local/share/aqtion/check_install.sh
@@ -6,9 +6,28 @@ AQTION_DIR=${HOME}/aqtion
 DISTRIB_URL="https://api.github.com/repos/actionquake/distrib/releases/latest"
 ARCH=$(uname -m)
 
+## Argument script logic
+
 if [ $1 = "clean" ]
 then
     rm -rf ${AQTION_DIR}/aqtion/versions
+fi
+
+if [ $1 = "update" ]
+then
+    if [ ! -f "${AQTION_DIR}/versions" ]
+    then
+        echo "No local install found, downloading the latest version..."
+        check_for_install
+    else
+        INSTALLED_VERSION=$(grep -s installed_version ${AQTION_DIR}/versions | cut -f 2 -d "=")
+        check_for_updates ${INSTALLED_VERSION}
+    fi
+fi
+
+if [ $1 = "uninstall" ]
+then
+    uninstall
 fi
 
 ## Check if current context is root, do not install if root
@@ -50,7 +69,10 @@ check_for_install () {
     then
         download_aqtion
     else
-        check_for_updates ${INSTALLED_VERSION}
+        echo "Existing installation found!"
+        echo "If you wish to upgrade to a newer version if one is available, re-run like so:"
+        echo "aqtion update"
+        exit 0
     fi
 }
 
@@ -102,10 +124,13 @@ download_aqtion () {
         echo "uname: $(uname)"
         echo "arch detected: $(uname -m)"
         exit 1
-    fi 
+    fi
 
-    LATEST_PACKAGE=$(curl -q -s ${DISTRIB_URL} | grep browser_download_url | cut -d '"' -f 4 | grep ${LINUX_ARCH} | grep client | grep -v deb | head -n 1)
+    ## Tarball name "aqtion-client-VERSION-linux-ARCH.tar.gz"
+
+    #LATEST_PACKAGE=$(curl -q -s ${DISTRIB_URL} | grep browser_download_url | cut -d '"' -f 4 | grep ${LINUX_ARCH} | grep client | grep -v deb | head -n 1)
     LATEST_VERSION=$(curl -q -s ${DISTRIB_URL} | grep browser_download_url | cut -d '"' -f 4 | grep ${LINUX_ARCH} | grep client | grep -v deb | head -n 1 | cut -d "/" -f 8)
+    LATEST_PACKAGE="aqtion-client-${LATEST_VERSION}-linux-${LINUX_ARCH}.tar.gz"
     echo "Downloading AQtion ${LATEST_VERSION} ..."
     curl --progress-bar -q -s -L -o /tmp/aqtion_latest.tar.gz "${LATEST_PACKAGE}"
     tar xzf /tmp/aqtion_latest.tar.gz -C "${AQTION_DIR}" --strip-components=1
@@ -115,15 +140,34 @@ download_aqtion () {
         echo "Installation successful!"
         launch_game
     else
+        if [ -z "${LATEST_VERSION}" ] || [ -z "${LATEST_PACKAGE}" ]
+        then
+            LATEST_VERSION="undefined"
+            LATEST_PACKAGE="undefined"
+        fi
         echo "Installation failure, take this debug information and post a Github Issue at https://github.com/actionquake/distrib with the following information: "
         echo "Latest version available: ${LATEST_VERSION}"
         echo "Attempted package download: ${LATEST_PACKAGE}"
     fi
 }
 
+uninstall () {
+    echo "Completely removing AQtion from ${AQTION_DIR} ..."
+    rm -rf ${AQTION_DIR}
+    if [ $? = "0" ]
+    then
+        echo "Removal successful"
+        exit 0
+    else
+        echo "Error in removing files, check that they are not in use or that you don't have a shell that is in ${AQTION_DIR} directory"
+        echo "To manually uninstall, run 'rm -rf ${AQTION_DIR}' in your shell"
+        exit 1
+    fi
+}
+
 launch_game () {
-    cd ${AQTION_DIR}
-    ${AQTION_DIR}/q2pro "$@"
+    cd ${AQTION_DIR} || return
+    ${AQTION_DIR}/q2pro
 }
 
 # Let's-a go
