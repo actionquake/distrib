93b42932c251b095f9534f37ed2d2d29556582c4
Dino <8dino2@gmail.com>
2022-04-17 20:40:27 -0400

    Updated dmg_mac for an optional upload to release via script

--- build/mac/dmg_mac.sh
+++ build/mac/dmg_mac.sh
@@ -27,19 +27,15 @@ echo "Version: ${VERSION}"
 ## Change dynamic lib references manually because Apple is dumb and won't let us static link
 echo "Adjusting dynamic lib paths for ${ARCH}..."
 if [[ ${ARCH} = "m1" ]]; then
-    #install_name_tool -change /usr/lib/libcurl.4.dylib @executable_path/.lib/libcurl.4.dylib q2probuilds/${ARCH}/q2pro
     install_name_tool -change /System/Library/Frameworks/OpenAL.framework/Versions/A/OpenAL @executable_path/.lib/libopenal.1.21.1.dylib q2probuilds/${ARCH}/q2pro
     install_name_tool -change /opt/homebrew/opt/libpng/lib/libpng16.16.dylib @executable_path/.lib/libpng16.16.dylib q2probuilds/${ARCH}/q2pro
     install_name_tool -change /opt/homebrew/opt/jpeg/lib/libjpeg.9.dylib @executable_path/.lib/libjpeg.9.dylib q2probuilds/${ARCH}/q2pro
     install_name_tool -change /usr/lib/libz.1.dylib @executable_path/.lib/libz.1.2.8.dylib q2probuilds/${ARCH}/q2pro
-    #install_name_tool -change /opt/homebrew/opt/sdl2/lib/libSDL2-2.0.0.dylib @executable_path/.lib/libSDL2-2.0.0.dylib q2probuilds/${ARCH}/q2pro
 else
-    #install_name_tool -change /usr/lib/libcurl.4.dylib @executable_path/.lib/libcurl.4.dylib q2probuilds/${ARCH}/q2pro
     install_name_tool -change /usr/local/opt/openal-soft/lib/libopenal.1.dylib @executable_path/.lib/libopenal.1.21.1.dylib q2probuilds/${ARCH}/q2pro
     install_name_tool -change /usr/local/opt/libpng/lib/libpng16.16.dylib @executable_path/.lib/libpng16.16.dylib q2probuilds/${ARCH}/q2pro
     install_name_tool -change /usr/local/opt/jpeg/lib/libjpeg.9.dylib @executable_path/.lib/libjpeg.9.dylib q2probuilds/${ARCH}/q2pro
     install_name_tool -change /usr/lib/libz.1.dylib @executable_path/.lib/libz.1.2.11.dylib q2probuilds/${ARCH}/q2pro
-    #install_name_tool -change /usr/local/opt/sdl2/lib/libSDL2-2.0.0.dylib @executable_path/.lib/libSDL2-2.0.0.dylib q2probuilds/${ARCH}/q2pro
 fi
 
 ## create MacOS if it does not exist
@@ -55,10 +51,18 @@ cp q2probuilds/${ARCH}/game*.so AQ_Install/AQ.app/Contents/MacOS/action/
 chmod +x AQ_Install/AQ.app/Contents/MacOS/q2pro*
 
 ## Create dmg file
-hdiutil create -ov aqtion-client-${VERSION}-mac-${ARCH}.dmg -srcfolder AQ_Install -volname "Action Quake"
+hdiutil create -ov aqtion-client-${VERSION}-mac-${ARCH}.dmg -srcfolder AQ_Install -volname "AQtion"
 
 ## Move action folder back
 mv AQ_Install/AQ.app/Contents/MacOS/action ../../
 
 ## Delete MacOS-folder
 rm -r -f AQ_Install/AQ.app/Contents/MacOS
+
+## Optional upload directly to the release
+read -p "Do you want to automatically upload aqtion-client-${VERSION}-mac-${ARCH}.dmg to an existing Github Release? (Y/N):  " yn
+case $yn in
+    [Yy]* ) gh release upload ${VERSION} aqtion-client-${VERSION}-mac-${ARCH}.dmg;;
+    [Nn]* ) exit 0;;
+    * ) echo "Please answer Y or N.";;
+esac
