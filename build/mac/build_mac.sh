4adad8756b7d02c27a8a58610df9a90009ec1cdf
Dino <8dino2@gmail.com>
2022-04-28 09:35:36 -0400

    Cleaned up build script a bit

--- build/mac/build_mac.sh
+++ build/mac/build_mac.sh
@@ -2,13 +2,13 @@
 
 RAW_ARCH=$1
 CURRENT_DIR=$(pwd)
+PKG_CONFIG_PATH="/usr/local/Cellar/openal-soft/1.21.1/lib/pkgconfig/"
+CONFIG_FILE=config_mac
 
 if [[ -z ${RAW_ARCH} ]]
 then
     echo "How to use this script:"
-    echo "./build_mac.sh [intel|m1] [standalone|steam]"
-    echo "The first argument for building for intel or m1 processors"
-    echo "The second argument for building standalone or Steam packages"
+    echo "./build_mac.sh [intel | m1]"
     echo "Re-run with the appropriate arguments"
     exit 1
 fi
@@ -24,29 +24,26 @@ echo "Building for ${ARCH}"
 echo "Current dir is ${CURRENT_DIR}"
 echo "Proceeding with build..."
 
+### Q2Pro
+
 Q2PRO_DIR="q2pro"
-## Cleanup /tmp/q2pro
+
+## Cleanup /tmp/q2pro if exists
 rm -rf ${Q2PRO_DIR}
 
 ## Clone repository, copy config file
 git clone https://github.com/skullernet/q2pro.git ${Q2PRO_DIR}
 cp config_mac_${ARCH} ${Q2PRO_DIR}/config_mac
 
-## Apple Silicon M1 needs a special Makefile
-#if [[ ${ARCH} = "m1" ]]; then
-#    cp aq2tng_Makefile_mac_m1 ${Q2PRO_DIR}/Makefile
-#    echo "Copying m1 Makefile successful"
-#fi
-
 ## Patch system.c patch file to make Mac paths work
 cp mac_dirpath.patch ${Q2PRO_DIR}/src/unix/
 cd ${Q2PRO_DIR}/src/unix
 patch < mac_dirpath.patch
-cd ../../../
+cd ${CURRENT_DIR}
 
 ## Build the q2pro binaries
 cd ${Q2PRO_DIR} || return
-export CONFIG_FILE=config_mac;export PKG_CONFIG_PATH="/usr/local/Cellar/openal-soft/1.21.1/lib/pkgconfig/";make -j2 V=1
+make -j2 V=1
 build_exitcode=$?
 
 ## Copy files in preparation for the build step
@@ -66,15 +63,10 @@ rm -rf ${CURRENT_DIR}/q2pro
 ## Generate dylib mappings
 cd ${CURRENT_DIR}
 mkdir -p q2probuilds/${ARCH}/lib
-if [[ ${ARCH} = "m1" ]]; then
-    dylibbundler -b -x "q2probuilds/${ARCH}/q2pro" \
-            -x "q2probuilds/${ARCH}/q2proded" \
-            -d "q2probuilds/${ARCH}/lib" -of -p @executable_path/m1lib
-else
-    dylibbundler -b -x "q2probuilds/${ARCH}/q2pro" \
-            -x "q2probuilds/${ARCH}/q2proded" \
-            -d "q2probuilds/${ARCH}/lib" -of -p @executable_path/intellib
-fi
+
+dylibbundler -b -x "q2probuilds/${ARCH}/q2pro" \
+        -x "q2probuilds/${ARCH}/q2proded" \
+        -d "q2probuilds/${ARCH}/lib" -of -p @executable_path/${ARCH}lib
 
 ## make q2pro executable
 chmod +x q2probuilds/${ARCH}/q2pro*
